7cb233a15668fc8cc74e4178e46413d752416020
Austin Howard <cnfishead@hotmail.com>
2025-06-09 19:16:01 -0400

    fetching plans

--- src/layout/billingSetupModal/FeatureSelect.component.tsx
+++ src/layout/billingSetupModal/FeatureSelect.component.tsx
@@ -1,12 +1,24 @@
 'use client';
 import { Button } from 'antd';
 import styles from './FeatureSelect.module.scss';
+import { useUser } from '@/state/auth';
+import useApiHook from '@/hooks/useApi';
 
 type Props = {
   onContinue: () => void;
 };
 
 const FeatureSelect = ({ onContinue }: Props) => {
+  const { data: loggedInUser } = useUser();
+
+  const { data: plansRequest, isLoading } = useApiHook({
+    url: `/auth/plan`,
+    key: 'plan-select',
+    method: 'GET',
+    enabled: !!loggedInUser._id,
+    filter: `availableTo;{"$in":"${Object.keys(loggedInUser.profileRefs).join(',')}"}`,
+  }) as any;
+
   return (
     <div className={styles.container}>
       <p>Select your desired features (placeholder)</p>
