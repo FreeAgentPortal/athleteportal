be6701364d601c95c8d0c5cebec592d4b60ea07e
Austin Howard <cnfishead@hotmail.com>
2025-06-14 14:56:38 -0400

    error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import PageLayout from '@/layout/page/Page.layout';
-import styles from './page.module.scss';
 import { navigation } from '@/data/navigation';
 import Dashboard from '@/views/dashboard/Dashboard.view';
 
