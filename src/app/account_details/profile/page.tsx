b1d69478911da38c88d945de4d0aa0541a76cdc7
Austin Howard <cnfishead@hotmail.com>
2025-06-15 12:13:40 -0400

    laying out profile page

--- src/app/account_details/profile/page.tsx
+++ src/app/account_details/profile/page.tsx
@@ -1,19 +1,19 @@
-import PageLayout from "@/layout/page/Page.layout";
-import { navigation } from "@/data/navigation";
-
+import PageLayout from '@/layout/page/Page.layout';
+import { navigation } from '@/data/navigation';
+import Profile from '@/views/profile/Profile.view';
 
 export const metadata = {
-  title: "FAP | Profile",
-  description: "Edit your profile details and preferences.",
+  title: 'FAP | Profile',
+  description: 'Edit your profile details and preferences.',
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
 };
 
 export default function Page() {
   return (
     <PageLayout pages={[navigation().account_details.links.profile]}>
-      <></>
+      <Profile />
     </PageLayout>
   );
 }
--- src/views/profile/Profile.view.tsx
+++ src/views/profile/Profile.view.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import styles from './Profile.module.scss';
+import { Tabs, TabsProps } from 'antd';
+import BasicInfo from './subViews/basicInfo/BasicInfo.component';
+import Measurements from './subViews/measurements/Measurements.component';
+import PerformanceMetrics from './subViews/perfMetrics/PerformanceMetrics.component';
+import Background from './subViews/background/Background.component';
+import Media from './subViews/media/Media.component';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const tabs: TabsProps['items'] = [
+    {
+      label: 'Info',
+      key: '1',
+      children: <BasicInfo />,
+    },
+    {
+      label: 'Measurements',
+      key: '2',
+      children: <Measurements />,
+    },
+    {
+      label: 'Performance Metrics',
+      key: '3',
+      children: <PerformanceMetrics />,
+    },
+    {
+      label: 'Background',
+      key: '4',
+      children: <Background />,
+    },
+    {
+      label: 'Media',
+      key: '5',
+      children: <Media />,
+    },
+  ];
 
-export default Profile
+  return (
+    <div className={styles.container}>
+      <Tabs defaultActiveKey="1" type="card" items={tabs} animated />
+    </div>
+  );
+};
+
+export default Profile;
